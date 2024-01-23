6282c02cd05d49b80fab181a86392deb2f9d4ae4
nellyGuo1225 <forwork28957@gmail.com>
2024-01-23 16:53:34 +0800

    新增圖片按鈕沒反應問題 調整

--- JS/product.js
+++ JS/product.js
@@ -12,48 +12,32 @@ if(token === "" || token === null){
     window.location.href="./login.html"
 }
 
+let updateModal = null;
+let delModal = null;
+let myToast = null;
+
+
 const app = createApp({
     data() {
         return {
             tempProduct: {
-                data: {}
+                imagesUrl:[]
             },
+            product: {},
             products: [],
-            product: {
-                data: {
-                    title: '',
-                    category: '',
-                    origin_price: 0,
-                    price: 0,
-                    unit: '個',
-                    description: '',
-                    content: '',
-                    is_enabled: 1,
-                    imageUrl: 'https://images.unsplash.com/photo-1516550135131-fe3dcb0bedc7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=621e8231a4e714c2e85f5acbbcc6a730&auto=format&fit=crop&w=1352&q=80',
-                    imagesUrl: [
-                        'https://images.unsplash.com/photo-1516627145497-ae6968895b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1924&q=80',
-                        'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-                        'https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1948&q=80',
-                        'https://images.unsplash.com/photo-1617093727343-374698b1b08d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-                        'https://images.unsplash.com/photo-1511914265872-c40672604a80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80'
-                    ]
-                }
-            },
-            addProductModal: {},
-            editProductModal: {},
-            myToast: {},
-            toastContent: 'Hi'
+            isNew: false,
+            toastContent:'Hi'
         }
     },
     methods: {
         getProducts() {
             axios.get(`${url}/api/${path}/admin/products`)
                 .then((res) => {
-                    console.log(res);
                     this.products = res.data.products;
                 })
                 .catch((error) => {
                     console.dir(error);
+                    alert(error.data.message);
                     window.location.href="./login.html"
                 })
         },
@@ -64,64 +48,86 @@ const app = createApp({
                 })
                 .catch((error) => {
                     console.dir(error);
+                    alert(error.data.message);
                     window.location.href="./login.html"
                 })
         },
+        openModal(status, item) {
+            if(status === 'new') {
+                this.tempProduct = {
+                    imagesUrl:[]
+                }
+                this.isNew = true;
+                updateModal.show();
+                
+            }else if(status === 'edit') {
+                this.tempProduct = {
+                    imagesUrl:[],
+                    ...item,
+                };
+                this.isNew = false;
+                updateModal.show();
+            }else {
+                this.tempProduct = {...item};
+                delModal.show();
+            }
+            
+        },
         createNewProduct() {
+            this.product.data = this.tempProduct;
             axios.post(`${url}/api/${path}/admin/product`,this.product)
                 .then((res) => {
-                    this.addProductModal.hide();
+                    updateModal.hide();
                     this.toastContent = '產品建立成功。';
-                    this.myToast.show();
+                    myToast.show();
                     this.getProducts();
                 })
                 .catch((error) => {
-                    console.dir(error);
+                    alert(error.data.message);
                 })
         },
-        deleteProduct(id) {
-            axios.delete(`${url}/api/${path}/admin/product/${id}`)
+        deleteProduct() {
+            axios.delete(`${url}/api/${path}/admin/product/${this.tempProduct.id}`)
                 .then((res) => {
                     this.toastContent = '成功刪除一筆資料。';
-                    this.myToast.show();
+                    myToast.show();
                     this.getProducts();
+                    delModal.hide();
                 })
                 .catch((error) => {
-                    console.dir(error);
+                    alert(error.data.message);
                 })
         },
-        prepareEdit(item) {
-            this.tempProduct.data = JSON.parse(JSON.stringify(item));
-        },
-        confirmEdit(id) {
-            axios.put(`${url}/api/${path}/admin/product/${id}`,this.tempProduct)
+        confirmEdit() {
+            this.product.data = this.tempProduct;
+            axios.put(`${url}/api/${path}/admin/product/${this.tempProduct.id}`, this.product)
                 .then((res) => {
-                    console.log(res.data);
                     this.toastContent = '修改成功。';
-                    this.editProductModal.hide();
-                    this.myToast.show();
+                    updateModal.hide();
+                    myToast.show();
                     this.getProducts();
                 })
                 .catch((error) => {
-                    console.dir(error);
+                    alert(error.data.message);
                 })
         }
     },
     mounted() {
+
         this.checkLogin();
         this.getProducts();
         
         //Modal
-        this.addProductModal = new bootstrap.Modal(document.getElementById('newProductModal'), {
+        updateModal = new bootstrap.Modal(document.getElementById('updateProductModal'), {
             keyboard: false,
           });
         
-        this.editProductModal = new bootstrap.Modal(document.getElementById('editModal'), {
+        delModal = new bootstrap.Modal(document.getElementById('delModal'), {
             keyboard: false,
           });
 
         //toast
-        this.myToast = new bootstrap.Toast(document.getElementById('myToast'));
+        myToast = new bootstrap.Toast(document.getElementById('myToast'));
     },
 });
 
